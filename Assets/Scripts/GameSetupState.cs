9c7c62ae15cce3195063e22d682c44a3e8132b76
Connor Gonzalez <connorgonzalez13@gmail.com>
2024-03-21 10:22:57 -0500

    Added Initial Setup State
    Transferred GameSetupState from Project01 to act as template state.

---
+++ Assets/Scripts/GameSetupState.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetupState : State
+{
+    private GameFSM _stateMachine;
+    private GameController _controller;
+
+    public GameSetupState(GameFSM stateMachine, GameController controller)
+    {
+        _stateMachine = stateMachine;
+        _controller = controller;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        Debug.Log("STATE: Setup");
+
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        Debug.Log("END: Setup");
+    }
+
+    public override void FixedTick()
+    {
+        base.FixedTick();
+    }
+
+    public override void Tick()
+    {
+        base.Tick();
+    }
+}
