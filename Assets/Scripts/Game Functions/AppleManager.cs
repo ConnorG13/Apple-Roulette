ea9414756e6b9d6b98a1c9666553e1a20e402c48
Connor Gonzalez <connorgonzalez13@gmail.com>
2024-04-04 10:37:18 -0500

    added apple functionality

--- Assets/Scripts/Game Functions/AppleManager.cs
+++ Assets/Scripts/Game Functions/AppleManager.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class AppleManager : MonoBehaviour
 {
+    [SerializeField] private int _maxApples = 6;
+    [SerializeField] private int _poisonApples = 1;
+    private int _currentApple = 0;
     private int _poolLength;
     private int _poisonCount;
     private int[] _Apples;
     
     void Start()
     {
-
         CreatePool();
     }
-
     public void CreatePool()
     {
-        _poolLength = Mathf.RoundToInt(Random.Range(4f, 8f));
-        _poisonCount = Mathf.RoundToInt(Random.Range(1f, (_poolLength - 2)));
+        _poolLength = _maxApples;
+        _poisonCount = _poisonApples;
         _Apples = new int[_poolLength];
 
         for (int t = 0; t < _poisonCount; t++)
@@ -44,5 +45,28 @@ public class AppleManager : MonoBehaviour
         {
             Debug.Log(t.ToString());
         }
+    }
+    public void ShufflePool()
+    {
+        for (int t = _currentApple; t < _Apples.Length; t++)
+        {
+            int hold = _Apples[t];
+            int r = Random.Range(t, _Apples.Length);
+            _Apples[t] = _Apples[r];
+            _Apples[r] = hold;
+        }
+    }
+    public void PullApple()
+    {
+        if (_Apples[_currentApple] == 0)
+        {
+            //0 = safe
+        }
+        else
+        {
+            //1 = poison
+        }
+
+        _currentApple++;
     }
 }
