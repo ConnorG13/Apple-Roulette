8748ebfcdee14366a2e3c9b38b69fd827555a02e
Connor G <cpg200001@utdallas.edu>
2024-04-03 23:54:18 -0500

    Randomization Step 1
    Number of apples + number of poison apples successfully able to be randomized.

--- Assets/Scripts/Game Functions/AppleManager.cs
+++ Assets/Scripts/Game Functions/AppleManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class AppleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int _poolLength;
+    private int _poisonCount;
+    private int[] _Apples;
+    
     void Start()
     {
-        
+
+        CreatePool();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreatePool()
     {
-        
+        _poolLength = Mathf.RoundToInt(Random.Range(4f, 8f));
+        _poisonCount = Mathf.RoundToInt(Random.Range(1f, (_poolLength - 2)));
+        _Apples = new int[_poolLength];
+
+        for (int t = 0; t < _poisonCount; t++)
+        {
+            _Apples[t] = 1;
+        }
+
+        for (int t = _poisonCount; t < _Apples.Length; t++)
+        {
+            _Apples[t] = 0;
+        }
+
+        foreach(int t in _Apples)
+        {
+            Debug.Log(t.ToString());
+        }
     }
 }
